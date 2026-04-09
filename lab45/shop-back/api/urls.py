e1c7133848df4aea7497517e51fe566e567042ac
melomilk <pakMV2006@gmail.com>
2026-04-09 09:28:32 +0500

    continued backend part for the prj

--- lab45/shop-back/api/urls.py
+++ lab45/shop-back/api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-
-from . import views
+from .views import (
+    CategoryDetailAPIView,
+    CategoryListAPIView,
+    CategoryProductsAPIView,
+    ProductDetailAPIView,
+    ProductListAPIView,
+)
 
 urlpatterns = [
-    path('products/', views.products_list),
-    path('products/<int:id>/', views.product_detail),
-    path('categories/', views.categories_list),
-    path('categories/<int:id>/', views.category_detail),
-    path('categories/<int:id>/products/', views.category_products_list),
+    path('products/', ProductListAPIView.as_view()),
+    path('products/<int:product_id>/', ProductDetailAPIView.as_view()),
+    path('categories/', CategoryListAPIView.as_view()),
+    path('categories/<int:category_id>/', CategoryDetailAPIView.as_view()),
+    path('categories/<int:category_id>/products/', CategoryProductsAPIView.as_view()),
 ]
